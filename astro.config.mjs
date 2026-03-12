e322acba3878413520dfe4a0935d214ef8993d61
mauricio.saldarriaga <mauricio.saldarriaga@gss.com.co>
2026-03-12 10:06:16 -0500

    feat: setup SEO sitemap, robots.txt and add google-site-verification meta tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://agendacultural-c4.com',
+  integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
